753def58dd024410c08e6cfe0c879b654255fb34
Franco <franco.acuna@consultr.net>
2023-04-18 19:16:39 -0300

    fix greeting text

--- src/components/Greeting/index.jsx
+++ src/components/Greeting/index.jsx
@@ -7,7 +7,7 @@ const Greeting = () => {
             <div className={styles.greeting_container}>
                 ¡Bienvenido a Paw Pets! <br/><br/>
                 Aquí te mostraremos todo lo que necesitas para cuidar a tu mejor amigo peludo. Desde juguetes y accesorios, hasta consejos de entrenamiento y nutrición, ¡lo tenemos todo! <br/><br/>
-                Explora nuestra selección cuidadosamente seleccionada de productos para asegurarte de que tu mascota tenga todo lo que necesita para ser feliz y saludable. <br/><br/>
+                Nuestros productos son de la más alta calidad para asegurarte de que tu mascota tenga todo lo que necesita para ser feliz y saludable. <br/><br/>
                 ¡Gracias por visitarnos!
             </div>
         )
